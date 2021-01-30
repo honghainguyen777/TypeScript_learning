0bc67ca55a1942ea7ff40a5101c505348ba3fa41
Hong Hai <59792233+honghainguyen777@users.noreply.github.com>
2021-01-30 21:09:25 +0100

    Type Basics

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,8 @@
-var character = 'hai';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) { return console.log('input'); });
+var character = 'Hai'; // this variable can now only be a string
+var age = 30; // age only now hold a number
+var isRich = false; // isRich now hold a boolean
+// a function
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
--- sandbox.ts
+++ sandbox.ts
@@ -1,5 +1,11 @@
-const character = 'hai';
-console.log(character);
-const inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(input => console.log('input'))
+let character = 'Hai' // this variable can now only be a string
+let age = 30; // age only now hold a number
+let isRich = false; // isRich now hold a boolean
+
+// a function
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI;
+}
+
+console.log(circ(7.5));
